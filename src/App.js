ed6f6d548f955bd0e19a665b4704d9ef56cf1feb
ngohuynhduc <ucngohuynh@gmail.com>
2022-03-09 17:24:09 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { TransferMember } from './Components/TransferMember';
+
+function App() {
+  return (
+    <div className="App">
+      <TransferMember/>
+    </div>
+  );
+}
+
+export default App;
